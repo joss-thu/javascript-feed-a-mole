1d3b3adf7b68657311a31ea33658e1aa21e9179f
joss-thu <antojo01@thu.de>
2024-09-18 14:17:04 +0200

    code-refactoring

--- script.js
+++ script.js
@@ -1,41 +1,45 @@
 
+const moleholes= document.querySelectorAll('.mole-hole');
+const viewPortDiv= document.querySelector('.viewport-div');
+const moleHoleWidth= moleholes[0].clientWidth;
+const moleHoleHeight= moleholes[0].clientHeight;
 
-
-document.addEventListener('DOMContentLoaded',()=>{
-//randomly calculate positions within the viewport
-//check if intersecting
-//if not, populate them with mole-holes
-
-    const moleholes= document.querySelectorAll('.mole-hole');
-    const viewPortDiv= document.querySelector('.viewport-div');
-    const moleHoleWidth= moleholes[0].clientWidth;
-    const moleHoleHeight= moleholes[0].clientHeight;
+function populateMoleHoles(){
+    //randomly calculate positions within the viewport
+    //check if intersecting
+    //if not, populate them with mole-holes
+    document.addEventListener('DOMContentLoaded',()=>{
+        let positions= [];
+        let moleholeNum=1;
     
+        positionMoleHole(moleholes, positions);
+    });
+}
 
-    let positions= [];
-
-    function isIntersecting(X,Y){
-        for(let position of positions){
-            if(
-                (X<position.X+moleHoleWidth) && (X+moleHoleWidth>position.X) &&
-                (Y<position.Y+moleHoleWidth) && (Y+moleHoleWidth>position.Y)){
-                    return true;
-                }
-        }
-        return false;
+function isIntersecting(X,Y,width,height,positions){
+    for(let position of positions){
+        if(
+            (X<position.X+width) && (X+width>position.X) &&
+            (Y<position.Y+height) && (Y+height>position.Y)){
+                return true;
+            }
     }
+    return false;
+}
 
-2
+function positionMoleHole(moleholes,positions){
     moleholes.forEach(molehole=>{
         let moleHoleX,moleHoleY;
         do{
             moleHoleX= Math.random() * (viewPortDiv.clientWidth-moleHoleWidth);
             moleHoleY= Math.random() * (viewPortDiv.clientHeight-moleHoleHeight);
-        }while(isIntersecting(moleHoleX,moleHoleY))
-
+        }while(isIntersecting(moleHoleX,moleHoleY,moleHoleWidth,moleHoleHeight,positions))
         molehole.style.left= `${moleHoleX}px`
         molehole.style.top= `${moleHoleY}px`
         positions.push({X:moleHoleX, Y:moleHoleY});
-});
-});
+    });
+}
 
+//---------------------------------------------------------------------------------
+populateMoleHoles();
+    
