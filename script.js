21860423b0ce4533297ea718ecbe9772db4f90a0
joss-thu <antojo01@thu.de>
2024-09-19 09:42:34 +0200

    The holes are generated randomly at random positions, the number of holes generated depends on viewport dimensions. Maaximum number of holes are generated as long as there is no intersection of holes.
    The background texture of holes are also different for each molehole.

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
 
-const moleholes= document.querySelectorAll('.mole-hole');
 const viewPortDiv= document.querySelector('.viewport-div');
-const moleHoleWidth= moleholes[0].clientWidth;
-const moleHoleHeight= moleholes[0].clientHeight;
+const holesMax=20;
+const numPositionAttemptsLimit=1000;
+let positions= [];
+
 
 function populateMoleHoles(){
     //randomly calculate positions within the viewport
     //check if intersecting
     //if not, populate them with mole-holes
     document.addEventListener('DOMContentLoaded',()=>{
-        let positions= [];
-        let moleholeNum=1;
-    
+        for(let i= 1; i<=holesMax-1;i++){
+            const moleHole= document.createElement('div');
+            moleHole.classList.add('mole-hole');
+            moleHole.id=`mole-hole-${i}`;
+            moleHole.style.backgroundImage= `url('./img/soil-${i%4}.png')`;
+            viewPortDiv.appendChild(moleHole);
+            
+            
+            console.log(`"url('./img/soil-${i%4}.png')"`)
+        }
+        moleholes= document.querySelectorAll('.mole-hole');
+        console.log(moleholes.length,)
         positionMoleHole(moleholes, positions);
     });
 }
@@ -30,13 +40,24 @@ function isIntersecting(X,Y,width,height,positions){
 function positionMoleHole(moleholes,positions){
     moleholes.forEach(molehole=>{
         let moleHoleX,moleHoleY;
+        let numPositionAttempts=0;
+
+        const moleHoleWidth= molehole.clientWidth;
+        const moleHoleHeight= molehole.clientHeight;
         do{
             moleHoleX= Math.random() * (viewPortDiv.clientWidth-moleHoleWidth);
             moleHoleY= Math.random() * (viewPortDiv.clientHeight-moleHoleHeight);
-        }while(isIntersecting(moleHoleX,moleHoleY,moleHoleWidth,moleHoleHeight,positions))
-        molehole.style.left= `${moleHoleX}px`
-        molehole.style.top= `${moleHoleY}px`
-        positions.push({X:moleHoleX, Y:moleHoleY});
+            numPositionAttempts++;
+           
+        }while((isIntersecting(moleHoleX,moleHoleY,moleHoleWidth,moleHoleHeight,positions)) &&(numPositionAttempts<=numPositionAttemptsLimit) )
+        if(numPositionAttempts<=numPositionAttemptsLimit){
+            molehole.style.left= `${moleHoleX}px`
+            molehole.style.top= `${moleHoleY}px`
+            positions.push({X:moleHoleX, Y:moleHoleY});
+        }else{
+            molehole.remove();
+        }
+
     });
 }
 
